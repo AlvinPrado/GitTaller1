c3186e5b93289050c46a77a159fcba56ff6b8bf8
Alvin Francisco Prado Peynado <alvinprado185@gmail.com>
2024-11-28 02:55:51 -0600

    Create trabajocurso.js

---
+++ trabajocurso.js
@@ -0,0 +1,11 @@
+console.log("Hola a todos chicos, aquí la multiplicación de los valores z(32) y x(11):");
+
+
+var z = 32;
+var x = 11;
+
+console.log(z * x);
+
+console.log("Nombre:");
+const Nombre = "Pabliyo";
+console.log("Hola, ¿qué tal amado " + Nombre + "?");
